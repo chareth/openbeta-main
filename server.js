534acc88ccdb7be4c44028def0cccfab5172adea
Viet Nguyen <vnguyen@redhat.com>
2016-06-16 19:40:31 -0700

    Use openshift envs if available

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 app.use(cors());
 app.use('/', express.static(__dirname + '/'));
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function () {
+  console.log('App listening on ' + server_ip_address + ":" + server_port);
 });
