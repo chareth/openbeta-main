1d3ed4c4aac13dfd8effe5d725603c04ce537dba
Viet Nguyen <vnguyen@redhat.com>
2016-12-27 20:50:30 -0600

    Added editor page

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var cors = require('cors');
 var app = express();
 
 app.use(function(req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
