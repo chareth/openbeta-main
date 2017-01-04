d5a0f4892f651871f525566b0d3b5fbd95ef7d5e
Viet Nguyen <viet2046@gmail.com>
2017-01-03 23:35:30 -0600

    Osm integration (#1)
    * Initial work
    * Added editor page

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', express.static(__dirname + '/'));
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
